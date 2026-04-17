da2f736d1e45ad1124dd745154b6bce5aaa1d25a
geoffroy gridel <geoffroy.gridel@gmail.com>
2026-04-17 15:52:39 +0200

    added somes changes to fast.py

--- app/ai_for_finance.py
+++ app/ai_for_finance.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import pandas as pd
 
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
@@ -16,3 +17,13 @@ def my_prediction_function(
         prediction = model.predict([[open_price, high, low, close, volume]])
 
     return prediction
+
+
+
+def xgboost_function(df:pd.DataFrame):
+    model_xgb_path = os.path.join(ROOT_PATH, 'models', 'xgb_pipeline.pkl')
+    with open(model_xgb_path, 'rb') as file:
+        model_xgb = pickle.load(file)
+        prediction_xgb = model_xgb.predict(df)
+
+    return prediction_xgb
--- app/api/fast.py
+++ app/api/fast.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
 
 app = FastAPI()
 from app.ai_for_finance import my_prediction_function
@@ -29,3 +30,27 @@ def root():
     return {
         'greeting': 'Hello'
     }
+
+
+
+@app.get("/predict")
+def xg_boost_predict( date_pivot :str = '2023-11-04', optional_user_date:str = '2026-04-01'):
+
+     # Charger le CSV
+    df = pd.read_csv("../data_folder/cache/BTC-USD.csv")
+
+
+    # Convertir la 1re colonne en datetime
+    df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0])
+
+    # String de date à partir duquel tu veux filtrer
+    date_str = date_pivot
+    # Convertir ce string en datetime
+    date_cut = pd.to_datetime(date_str)
+    # Garder toutes les lignes à partir de cette date
+    sub_df = df[df["date"] >= date_cut]
+
+
+    prediction_xgb, probability_xgb = xgboost_function(sub_df)
+
+    return {"prediction": int(prediction_xgb[-1]), "probability": float(probability_xgb[-1])}
