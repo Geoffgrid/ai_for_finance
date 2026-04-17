655eda67302fec506605cb943e42ea5a55335592
geoffroy gridel <geoffroy.gridel@gmail.com>
2026-04-17 17:03:39 +0200

    several changes to make the api working

--- app/ai_for_finance.py
+++ app/ai_for_finance.py
@@ -2,6 +2,11 @@ import os
 import pickle
 import pandas as pd
 
+
+import __main__
+from app.ml_logic.features import build_technical_features
+
+
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
 def my_prediction_function(
@@ -20,10 +25,14 @@ def my_prediction_function(
 
 
 
-def xgboost_function(df:pd.DataFrame):
+def xgboost_prediction_function(df:pd.DataFrame):
     model_xgb_path = os.path.join(ROOT_PATH, 'models', 'xgb_pipeline.pkl')
+
+    __main__.build_technical_features = build_technical_features
+
     with open(model_xgb_path, 'rb') as file:
         model_xgb = pickle.load(file)
         prediction_xgb = model_xgb.predict(df)
+        probability_xgb = model_xgb.predict_proba(df)[:, 1]  # Probabilité de la classe positive
 
-    return prediction_xgb
+    return prediction_xgb, probability_xgb
--- app/api/fast.py
+++ app/api/fast.py
@@ -3,7 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 app = FastAPI()
-from app.ai_for_finance import my_prediction_function
+from app.ai_for_finance import my_prediction_function, xgboost_prediction_function
+from app.ml_logic.features import build_technical_features
 
 # Cross-Origin Middleware Resource Sharing (CORS)
 app.add_middleware(
@@ -33,24 +34,25 @@ def root():
 
 
 
-@app.get("/predict")
+@app.get("/predict_xgboost")
 def xg_boost_predict( date_pivot :str = '2023-11-04', optional_user_date:str = '2026-04-01'):
 
      # Charger le CSV
-    df = pd.read_csv("../data_folder/cache/BTC-USD.csv")
+    df = pd.read_csv("data_folder/cache/BTC-USD.csv")
 
 
     # Convertir la 1re colonne en datetime
-    df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0])
+    #df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0])
+    df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
 
     # String de date à partir duquel tu veux filtrer
     date_str = date_pivot
     # Convertir ce string en datetime
     date_cut = pd.to_datetime(date_str)
     # Garder toutes les lignes à partir de cette date
-    sub_df = df[df["date"] >= date_cut]
+    sub_df = df[df["Date"] >= date_cut]
 
 
-    prediction_xgb, probability_xgb = xgboost_function(sub_df)
+    prediction_xgb, probability_xgb = xgboost_prediction_function(sub_df)
 
     return {"prediction": int(prediction_xgb[-1]), "probability": float(probability_xgb[-1])}
